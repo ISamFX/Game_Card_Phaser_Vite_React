a84d0eb754564b7b0fd3df300762a9cbee5543ba
ISamFX <isam4fx@gmail.com>
2025-04-20 08:09:10 +0300

    create build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Game_Card_Phaser_Vite_React",
+  base: "/asset/",
   plugins: [react()],
 });
