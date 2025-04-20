b7f83ac75d2d9af62727f6584772786c77f60ff4
ISamFX <isam4fx@gmail.com>
2025-04-20 22:33:06 +0300

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/Game_Card_Phaser_Vite_React",
-  plugins: [react()],
-});
