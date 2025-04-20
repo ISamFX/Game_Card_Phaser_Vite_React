3da0e3521cf9c9a4aad9a8c1be15c73964e5371e
ISamFX <isam4fx@gmail.com>
2025-04-20 10:43:23 +0300

    create dyploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Game_Card_Phaser_Vite_React",
+  base: "/Game_Card_Phaser_Vite_React/",
   plugins: [react()],
 });
 
