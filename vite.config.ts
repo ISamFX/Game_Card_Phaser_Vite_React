4cf75f043a15624089e8ba7b2f716d008cdb1c99
ISamFX <isam4fx@gmail.com>
2025-04-20 13:46:43 +0300

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/assets",
+  base: "/Game_Card_Phaser_Vite_React/", 
   plugins: [react()],
 });
